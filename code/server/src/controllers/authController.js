6a88ce034898efb5f5db6c904788ed248701744a
Yasiru Raveen <e23161@eng.pdn.ac.lk>
2026-02-26 19:44:53 +0530

    yasiru: initialize loginAPI with secret key

--- code/server/src/controllers/authController.js
+++ code/server/src/controllers/authController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const userRepo = require('../repositories/userRepo');
+const jwt = require('jsonwebtoken');
 
 /**
  * we use bycrypt because if a hacker steals our database, they will only see randomized hash strings, 
@@ -35,4 +36,43 @@ const register = async (req, res) => {
     }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // 1. Find the user by email
+        const user = await userRepo.findUserByEmail(email);
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        // 2. Compare the provided password with the hashed password in the DB
+        const isMatch = await bcrypt.compare(password, user.password_hash);
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        // 3. Create a JWT (Digital Passport)
+        const token = jwt.sign(
+            { id: user.id, role: user.role }, // Data to hide in the token
+            process.env.JWT_SECRET,           // Our secret key
+            { expiresIn: '1d' }               // Token expires in 1 day
+        );
+
+        // 4. Send the token and user info back (exclude the password!)
+        res.status(200).json({
+            message: 'Login successful!',
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+                role: user.role
+            }
+        });
+
+    } catch (error) {
+        console.error('Login Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+module.exports = { register, login};
--- code/server/src/routes/authRoutes.js
+++ code/server/src/routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/authController');
 
 // When a POST request hits /register, trigger the controller function
 router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 module.exports = router;
