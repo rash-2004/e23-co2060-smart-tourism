eb2f9db28198b06fb5885c2672bbc494ec628a6b
DevminaK <e23187@eng.pdn.ac.lk>
2026-04-29 16:09:57 +0530

    Removed unwanted files

--- code/server/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// When a POST request hits /register, trigger the controller function
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
