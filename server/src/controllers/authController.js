7f4a5c812d51bb55383d77ad6e773ee2fbcf2c39
Ravindu <e23300@eng.pdn.ac.lk>
2026-07-19 20:01:54 +0530

    Fix Nodemailer ETIMEDOUT on Render

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -11,7 +11,9 @@ const pendingLogins = new Map();
 
 // Configure Nodemailer transporter
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
